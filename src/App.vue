8b312728bb60e0b1c4b3719c0dd80f269b9f7907
Isaac Mann <isaacplmann@gmail.com>
2024-05-27 12:52:25 -0400

    small tweaks

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+// import { Products } from 'products'
 </script>
 
 <template>
@@ -9,6 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+      <!-- <Products /> -->
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx(), nxViteTsPaths()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
